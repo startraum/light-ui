75997566ed6e0e6b2b0c567a41e9e1c1d52d8f82
Max Nowack <max@unsou.de>
2019-02-06 23:22:06 +0100

    Show test light in dev mode

--- server/lights.ts
+++ server/lights.ts
@@ -1,31 +1,32 @@
 import { Light } from '../components/lightState'
 
-const lights: { [key: string]: Light } = {
-  // default: {
-  //   name: 'default',
-  //   id: 'default',
-  //   hue: 0,
-  //   lightness: 0,
-  //   intensity: 0,
-  //   power: true,
-  //   colors: [{
-  //     hue: 0,
-  //     intensity: 100,
-  //     lightness: 50,
-  //   }, {
-  //     hue: 50,
-  //     intensity: 100,
-  //     lightness: 60,
-  //   }, {
-  //     hue: 150,
-  //     intensity: 75,
-  //     lightness: 80,
-  //   }, {
-  //     hue: 327.39,
-  //     lightness: 50,
-  //     intensity: 100,
-  //   }],
-  // },
+const lights: { [key: string]: Light } = {}
+if (process.env.NODE_ENV !== 'production') {
+  lights.default = {
+    name: 'default',
+    id: 'default',
+    hue: 0,
+    lightness: 0,
+    intensity: 0,
+    power: true,
+    colors: [{
+      hue: 0,
+      intensity: 100,
+      lightness: 50,
+    }, {
+      hue: 50,
+      intensity: 100,
+      lightness: 60,
+    }, {
+      hue: 150,
+      intensity: 75,
+      lightness: 80,
+    }, {
+      hue: 327.39,
+      lightness: 50,
+      intensity: 100,
+    }],
+  }
 }
 
 export function getLights() {
