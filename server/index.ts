24b273730d9820058f6b59e84f50d24f40b951c1
Max Nowack <max@unsou.de>
2019-03-18 19:26:49 +0100

    Remove logging

--- server/index.ts
+++ server/index.ts
@@ -16,16 +16,13 @@ const subscriber = new Subscriber({ name: 'lightsBroadcast' })
 const publisher = new Publisher({ name: 'lightsBroadcast' })
 subscriber.on('light', (event: LightEvent) => {
   updateLight(event.light)
-  console.log('updated', event.light)
   io.emit('lights', getLights())
 })
 
 io.on('connection', socket => {
-  console.log('client connected', socket.handshake.address)
   socket.emit('lights', getLights())
 
   socket.on('update', ({ id, update }: { id: string, update: LightUpdate }) => {
-    console.log('control', id, update)
     patchLight(id, update)
     // @ts-ignore
     publisher.publish<LightUpdateEvent>('update', { id, update })
