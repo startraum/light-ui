9f1e0f277b55673181692a1122c7ec836f8efff4
Max Nowack <max@unsou.de>
2019-04-09 09:20:22 +0200

    Switch to redis pub sub

--- server/index.ts
+++ server/index.ts
@@ -1,30 +1,22 @@
-import { Light } from '../components/lightState'
-import { Publisher, Subscriber } from 'cote'
+import { subscribe, publish } from './redis'
 import io from './socketio'
 import { updateLight, patchLight, getLights, LightUpdate } from './lights'
 
-interface LightEvent extends Event {
-  light: Light
-}
-
-interface LightUpdateEvent extends Event {
-  id: string
-  update: LightUpdate
-}
-
-const subscriber = new Subscriber({ name: 'lightsBroadcast' })
-const publisher = new Publisher({ name: 'lightsBroadcast' })
-subscriber.on('light', (event: LightEvent) => {
-  updateLight(event.light)
+subscribe(light => {
+  updateLight(light)
   io.emit('lights', getLights())
 })
+
+const logAndCrash = (error: Error) => {
+  console.error(error)
+  process.exit(1)
+}
 
 io.on('connection', socket => {
   socket.emit('lights', getLights())
 
   socket.on('update', ({ id, update }: { id: string, update: LightUpdate }) => {
     patchLight(id, update)
-    // @ts-ignore
-    publisher.publish<LightUpdateEvent>('update', { id, update })
+    publish({ id, update }).catch(logAndCrash)
   })
 })
---
+++ server/redis.ts
@@ -0,0 +1,33 @@
+import { promisify } from 'util'
+import { createClient } from 'redis'
+import { Light } from '../components/lightState'
+import { LightUpdate } from './lights'
+
+if (!process.env.REDIS_URL) throw new Error('REDIS_URL not provided!')
+
+const subscriber = createClient({
+  url: process.env.REDIS_URL,
+})
+const publisher = createClient({
+  url: process.env.REDIS_URL,
+})
+
+subscriber.subscribe('light-updated')
+
+interface LightUpdateEvent {
+  id: string
+  update: LightUpdate
+}
+
+export function subscribe(fn: (light: Light) => void) {
+  subscriber.on('message', (channel, message) => {
+    if (channel !== 'light-updated') return
+    const light: Light = JSON.parse(message)
+    fn(light)
+  })
+}
+
+const p = promisify(publisher.publish.bind(publisher))
+export function publish({ id, update }: { id: string, update: LightUpdate }) {
+  return p('update-light', JSON.stringify({ id, update }))
+}
