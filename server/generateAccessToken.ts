70d4ca430877b58a303b8c60491c784661e54b30
Max Nowack <max@unsou.de>
2019-04-16 16:16:04 +0200

    Set cookie expiration to max date

--- server/generateAccessToken.ts
+++ server/generateAccessToken.ts
@@ -6,7 +6,7 @@ function randomId() {
 }
 
 export default (res: ServerResponse, expires?: Date) => {
-  res.setHeader('set-cookie', serialize('accessToken', randomId() , { expires }))
+  res.setHeader('set-cookie', serialize('accessToken', randomId() , { expires: expires || new Date(8640000000000000) }))
   if (expires) res.setHeader('set-cookie', serialize('guest', '1' , { expires }))
   res.setHeader('location', '/light')
   res.statusCode = 302
