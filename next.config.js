5efe99ea39c61bc898b1fedf7bd6458063ad3cd1
Max Nowack <max@unsou.de>
2019-04-18 09:47:23 +0200

    Fix linting

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
-const { parsed: localEnv } = require('dotenv').config()
-const webpack = require('webpack')
+const { parsed: localEnv } = require('dotenv').config();
+const webpack = require('webpack');
 const withTypescript = require('@zeit/next-typescript');
 module.exports = withTypescript({
   webpack(config) {
-    config.plugins.push(new webpack.EnvironmentPlugin(localEnv))
-    return config
+    config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
+    return config;
   },
 });
-
-console.log(module.exports)
